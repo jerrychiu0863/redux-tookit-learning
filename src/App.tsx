5e5a08ce3c26f76f7cd47f967269afa4941cc37c
Jerry Chiu <m08632000@hotmail.com>
2024-06-28 13:30:16 +0800

    wire up redux and react via typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { useDispatch, useSelector } from "react-redux";
-import type { RootState } from "./store";
+import { useAppSelector, useAppDispatch } from "./hooks";
 import CarForm from "./components/CarForm";
 import CarList from "./components/CarList";
 import CarSearch from "./components/CarSearch";
 import CarValue from "./components/CarValue";
+import { changeName } from "./store";
 
 export default function App() {
-  const dispatch = useDispatch();
-  const form = useSelector((state: RootState) => {
-    return state.form
-  })
+  const dispatch = useAppDispatch()
+  const form = useAppSelector((state) => state.form)
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(changeName(e.target.value))
+  }
   console.log(form)
   return (
     <div>
+      <input onChange={handleInputChange} />
       <CarForm />
       <CarSearch />
       <CarList />
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -8,4 +8,7 @@ const store = configureStore({
 })
 
 export { store, changeName, changeCost }
+// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
